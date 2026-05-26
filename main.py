551f40a6171a421a2a4c5a9bcf6f042da73fe65b
Roshni kumari <136426769+roshnisharma1111@users.noreply.github.com>
2026-05-26 14:18:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,52 +4,50 @@ import time
 
 app = Flask(__name__)
 
-latest_frame = None
-frame_lock = threading.Lock()
-
+latest_frame=None
+frame_lock=threading.Lock()
 
 @app.route("/")
 def home():
-    return "Flask server running"
+    return "Flask running"
 
-
-@app.route("/upload", methods=["POST"])
+@app.route("/upload",methods=["POST"])
 def upload():
+
     global latest_frame
 
     with frame_lock:
-        latest_frame = request.data
+        latest_frame=request.data
 
-    return "Frame received", 200
-
+    return "OK",200
 
 @app.route("/live")
 def live():
 
     def generate():
-        global latest_frame
 
         while True:
-            frame = None
 
             with frame_lock:
-                frame = latest_frame
+                frame=latest_frame
 
             if frame is not None:
-                yield (
+
+                yield(
                     b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' +
-                    frame +
+                    b'Content-Type:image/jpeg\r\n\r\n'
+                    +frame+
                     b'\r\n'
                 )
 
-            time.sleep(0.05)
+            time.sleep(0.1)
 
     return Response(
         generate(),
-        mimetype='multipart/x-mixed-replace; boundary=frame'
+        mimetype=
+        'multipart/x-mixed-replace; boundary=frame'
     )
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
