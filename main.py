bbadd5a870f667e6322984489595115a8c6a3393
Roshni kumari <136426769+roshnisharma1111@users.noreply.github.com>
2026-05-27 11:25:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,50 +4,63 @@ import time
 
 app = Flask(__name__)
 
-latest_frame=None
-frame_lock=threading.Lock()
+latest_frame = None
+frame_lock = threading.Lock()
 
 @app.route("/")
 def home():
     return "Flask running"
 
-@app.route("/upload",methods=["POST"])
+@app.route("/upload", methods=["POST"])
 def upload():
 
     global latest_frame
 
-    with frame_lock:
-        latest_frame=request.data
+    frame = request.data
 
-    return "OK",200
+    if frame:
+
+        with frame_lock:
+            latest_frame = frame
+
+    return "OK", 200
 
 @app.route("/live")
 def live():
 
     def generate():
 
+        global latest_frame
+
         while True:
 
             with frame_lock:
-                frame=latest_frame
+                frame = latest_frame
 
             if frame is not None:
 
-                yield(
+                yield (
                     b'--frame\r\n'
-                    b'Content-Type:image/jpeg\r\n\r\n'
-                    +frame+
+                    b'Content-Type: image/jpeg\r\n\r\n' +
+                    frame +
                     b'\r\n'
                 )
 
-            time.sleep(0.1)
+            time.sleep(0.03)
 
     return Response(
         generate(),
-        mimetype=
-        'multipart/x-mixed-replace; boundary=frame'
+        mimetype='multipart/x-mixed-replace; boundary=frame',
+        headers={
+            "Cache-Control": "no-cache",
+            "Pragma": "no-cache",
+            "Connection": "keep-alive"
+        }
     )
 
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        threaded=True
+    )
